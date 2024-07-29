19d713d4549fd41c42eebc1d8f1abe9c636f41b3
Nick Yoon <nicholas.yoon@yum.com>
2024-07-29 13:38:21 -0700

    Adding function

--- Sources/MySamplePackage/MySamplePackage.swift
+++ Sources/MySamplePackage/MySamplePackage.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct MySamplePackageHelper {
+    public init() {}
+    
+    public func print() -> String {
+        return "Hello world"
+    }
+}
